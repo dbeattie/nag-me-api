2e14dba234874a8ec82e1173983340a4c6e34e2a
Darren Beattie <darren@fettle.ca>
2020-02-18 16:16:57 -0500

    add cors comments

--- src/application.js
+++ src/application.js
@@ -34,7 +34,7 @@ function read(file) {
 }
 
 module.exports = function application(ENV) {
-  /*cors is currently hardcoded, it will fail if we push this condition to a new Heroku database or netlify --> Can create an array that should tolerate multiple URL's*/
+  /*cors is currently hardcoded, it will fail if we push this condition to a new Heroku database or netlify --> Can create an array that should tolerate multiple URL's but will need to look up documentation*/
   app.use(cors({origin: 'http://localhost:3000', credentials: true}));
   app.use(helmet());
   app.use(bodyparser.json());
