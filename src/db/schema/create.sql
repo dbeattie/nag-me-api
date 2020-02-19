991245426dbfd826f4eca5972143c146c4ae669b
Don-Stevenson <dsteven1@gmail.com>
2020-02-18 21:15:15 -0500

    phone numbers added to db

--- src/db/schema/create.sql
+++ src/db/schema/create.sql
@@ -6,21 +6,24 @@ CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
   email text UNIQUE NOT NULL,
-  password VARCHAR(255) NOT NULL
+  password VARCHAR(255) NOT NULL,
+  phone_number VARCHAR(15) NOT NULL
 );
 
 CREATE TABLE goals (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
-  user_id integer NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+  user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
   start_date DATE,
   end_date DATE,
-  cron VARCHAR(255)
+  cron VARCHAR(255),
+  friend_1_phone_number VARCHAR(15) NOT NULL,
+  friend_2_phone_number VARCHAR(15) NOT NULL
 );
 
 CREATE TABLE nags (
   id SERIAL PRIMARY KEY NOT NULL,
-  goal_id integer NOT NULL REFERENCES goals(id) ON DELETE CASCADE,
+  goal_id INTEGER NOT NULL REFERENCES goals(id) ON DELETE CASCADE,
   name VARCHAR(255) NOT NULL,
   completion BOOLEAN,
   date DATE,
--- src/db/schema/test.sql
+++ src/db/schema/test.sql
@@ -1,22 +1,65 @@
-
 -- Users table seeds --
-INSERT INTO users (name, email, password) VALUES ('Don Stevenson', 'Don@example.com', 'password');
-INSERT INTO users (name, email, password) VALUES ('Darren  Beattie', 'Darren en@example.com', 'password');
-INSERT INTO users (name, email, password) VALUES ('Kevin Zhu', 'Kevin@example.com', 'password');
+INSERT INTO users
+    (name, email, password, phone_number)
+VALUES
+    ('Don Stevenson', 'Don@example.com', 'password', '+14169090083');
+INSERT INTO users
+    (name, email, password, phone_number)
+VALUES
+    ('Darren Beattie', 'Darren@example.com', 'password', '+17788480760');
+INSERT INTO users
+    (name, email, password, phone_number)
+VALUES
+    ('Kevin Zhu', 'Kevin@example.com', 'password', '+14166487618');
 
--- Goals table seeds--
-INSERT INTO goals (name, start_date, end_date, cron) VALUES ('Learn the guitar', '2020-01-01', '2020-08-1', 'everyday at 1000');
-INSERT INTO goals (name, start_date, end_date, cron) VALUES ('Reading War and Peace', '2015-09-18', '2020-09-18', 'everyday at 1900');
-INSERT INTO goals (name, start_date, end_date, cron) VALUES ('Codewars', '2019-10-14', '2020-10-14', 'everyday at 900');
-
+-- Goals table seeds-- 
+INSERT INTO goals
+    (name, user_id, start_date, end_date, cron, friend_1_phone_number, friend_2_phone_number)
+VALUES
+    ('Learn the guitar', 1, '2020-01-01', '2020-08-1', 'everyday at 1000', '+17788480760', '+14166487618');
+INSERT INTO goals
+    (name, user_id, start_date, end_date, cron, friend_1_phone_number, friend_2_phone_number)
+VALUES
+    ('Reading War and Peace', 2, '2015-09-18', '2020-09-18', 'everyday at 1900', '+14166487618', '+14169090083');
+INSERT INTO goals
+    (name, user_id, start_date, end_date, cron, friend_1_phone_number, friend_2_phone_number)
+VALUES
+    ('Codewars', 3, '2019-10-14', '2020-10-14', 'everyday at 900', '+17788480760', '+14169090083');
 
 -- Nags table seeds --
-INSERT INTO nags (name, completion, date, time ) VALUES ('Did you practice guitar for 45 mins today?', 'true', '2020-02-27', '1000');
-INSERT INTO nags (name, completion, date, time ) VALUES ('Did you practice guitar for 45 mins today?', 'true', '2020-02-26', '1000');
-INSERT INTO nags (name, completion, date, time ) VALUES ('Did you practice guitar for 45 mins today?', 'true', '2020-02-25', '1000');
-INSERT INTO nags (name, completion, date, time ) VALUES ('Did you read War and Peace today?', 'true', '2020-02-27', '1900');
-INSERT INTO nags (name, completion, date, time ) VALUES ('Did you read War and Peace today?', 'false', '2020-02-26', '1900');
-INSERT INTO nags (name, completion, date, time ) VALUES ('Did you read War and Peace today?', 'true', '2020-02-25', '1900');
-INSERT INTO nags (name, completion, date, time ) VALUES ('Did you do CodeWars today?', 'true', '2020-02-27','900');
-INSERT INTO nags (name, completion, date, time ) VALUES ('Did you do CodeWars today?', 'false', '2020-02-26', '900');
-INSERT INTO nags (name, completion, date, time ) VALUES ('Did you do CodeWars today?', 'false', '2020-02-25', '900');
+INSERT INTO nags
+    (goal_id, name, completion, date, time )
+VALUES
+    (1, 'Did you practice guitar for 45 mins today?', 'true', '2020-02-27', '1000');
+INSERT INTO nags
+    (goal_id, name, completion, date, time )
+VALUES
+    (1, 'Did you practice guitar for 45 mins today?', 'true', '2020-02-26', '1000');
+INSERT INTO nags
+    (goal_id, name, completion, date, time )
+VALUES
+    (1, 'Did you practice guitar for 45 mins today?', 'true', '2020-02-25', '1000');
+INSERT INTO nags
+    (goal_id, name, completion, date, time )
+VALUES
+    (2, 'Did you read War and Peace today?', 'true', '2020-02-27', '1900');
+INSERT INTO nags
+    (goal_id, name, completion, date, time )
+VALUES
+    (2, 'Did you read War and Peace today?', 'false', '2020-02-26', '1900');
+INSERT INTO nags
+    (goal_id, name, completion, date, time )
+VALUES
+    (2, 'Did you read War and Peace today?', 'true', '2020-02-25', '1900');
+INSERT INTO nags
+    (goal_id, name, completion, date, time )
+VALUES
+    (3, 'Did you do CodeWars today?', 'true', '2020-02-27', '1100');
+INSERT INTO nags
+    (goal_id, name, completion, date, time )
+VALUES
+    (3, 'Did you do CodeWars today?', 'false', '2020-02-26', '1100');
+INSERT INTO nags
+    (goal_id, name, completion, date, time )
+VALUES
+    (3, 'Did you do CodeWars today?', 'false', '2020-02-25', '1100');
