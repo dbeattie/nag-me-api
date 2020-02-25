68987276d43073f5e6aa3665acc874963bc87484
Don-Stevenson <dsteven1@gmail.com>
2020-02-25 16:41:59 -0500

    handle query edge cases and update sms messages

--- src/helpers/sendMessages.js
+++ src/helpers/sendMessages.js
@@ -3,6 +3,7 @@ const authToken = process.env.authToken;
 const client = require("twilio")(accountSid, authToken);
 
 //*********EXAMPLE OF USER OBJECT TO SEND A NAG TO*****************/
+//*****************************************************************/
 // {
 //   id: 3,
 //   user_name: 'Kevin Zhu',
@@ -19,26 +20,19 @@ const client = require("twilio")(accountSid, authToken);
 // }
 
 // function to send out SMS messages to mulitiple [person] 6 AM
+//*************************************************************
 const sendSMSToMultiplePeople6AM = mulitiplePeople => {
   mulitiplePeople.forEach(sendSMSToNagUserOnly6AM);
 };
 
 // function to send out SMS messages to the user only 6 AM
+//********************************************************
 const sendSMSToNagUserOnly6AM = person => {
+  const todaysDate = new Date();
+  const dateDisplayOptions = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
   client.messages
     .create({
-      //get a real messasge from the body
-      body: `Greetings from Nag-Me.com!! Hello ${
-        person.user_name
-      }! You are working towards the goal of ${person.goal_name} ending on ${
-        person.end_date
-      }. Your goal for today is to complete the activity toward the goal of ${
-        person.goal_name
-      } by midnight on ${new Date().toLocaleDateString("en-us", {
-        weekday: "long",
-        month: "short",
-        day: "numeric"
-      })}. When you have completed the daily actvity, please log in to Nag-Me.com to mark today's Nag as complete.`,
+      body: `Greetings from Nag-Me.com!! Hello ${person.user_name}! You are working towards the goal of ${person.goal_name} ending on ${person.end_date.dateDisplayOptions}. Your goal for today is to complete the activity toward the goal of ${person.goal_name} by midnight on ${todaysDate.toLocaleDateString('en-us', dateDisplayOptions)}. When you have completed the daily actvity, please log in to Nag-Me.com to mark today's Nag as complete.`,
       from: "+13172155407",
       to: person.phone_number
     })
@@ -47,19 +41,24 @@ const sendSMSToNagUserOnly6AM = person => {
 };
 
 // function to send out SMS messages to mulitiple [person] 12AM
+//*************************************************************
 const sendSMSToMultiplePeople12AM = mulitiplePeople => {
   mulitiplePeople.forEach(sendSMSToNagUserOnly12AM);
 };
 
-// function currently only sends one to ONE person friend, despite multiple attempts to send to multiple
+// function currently only sends one nag completion result ONE person friend, despite multiple attempts to send to multiple people
+//********************************************************************************************************************************
 const sendSMSToNagUserOnly12AM = person => {
+  const todaysDate = new Date();
+  const dateDisplayOptions = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+  console.log("person is ", person)
   client.messages
     .create({
-      body: `Greetings from Nag-Me.com!! ${person.user_name} is working towards the goal of ${person.goal_name}. They did not complete their task for the day, ${person.user_name} requires an exorbitant amount of public shaming! `,
+      body: `Greetings from Nag-Me.com!! ${person.user_name} is working towards the goal of ${person.goal_name}. They did not complete their task for today, ${todaysDate.toLocaleDateString('en-us', dateDisplayOptions)}. ${person.user_name} requires an exorbitant amount of public shaming!`,
       from: "+13172155407",
       to: person.friend_1_phone_number
     })
-    .then(message => console.log(message.status))
+    .then(message => console.log("here is queued", message.status))
     .done();
 };
 
